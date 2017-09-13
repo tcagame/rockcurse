0886868c9c1c34e87384858833c4cd46e5d1e2fc
yoshida <y>
2017-09-13 16:21:31 +0900

    細かい修正

---
+++ Assets/Scripts/sound/BGM_vol1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_vol1 : MonoBehaviour {
+    // Use this for initialization
+
+    [SerializeField]
+    UnityEngine.Audio.AudioMixer mixer;
+
+    float BGM
+    {
+        set { mixer.SetFloat("BGM_Volume", Mathf.Lerp(-80, value)); }
+    }
+
+}
