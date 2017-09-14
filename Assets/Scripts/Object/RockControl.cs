24f4254895e2a81c35d9f35d01ec19e003237785
kent <kent.saito.jp@gmail.com>
2017-09-15 00:52:42 +0900

    リソースの追加と岩のアニメーション

--- Assets/Scripts/Object/RockControl.cs
+++ Assets/Scripts/Object/RockControl.cs
@@ -4,19 +4,66 @@ using UnityEngine;
 
 public class RockControl : MonoBehaviour {
 
-	float time = 8.0f;
+	Rigidbody2D rb2d;
+	SpriteRenderer sr;
+	Animator anim;
 
-	// Use this for initialization
-	void Start () {
-		
+	Color color;
+
+	const float GEN_TIME = 1.0f;
+	const float LIMIT_TIME = 2.0f;
+
+	float alpha;
+	float generate_time;
+	float destroy_time;
+	bool limit;
+
+	void Awake( ) {
+		rb2d = GetComponent< Rigidbody2D >( );
+		sr = GetComponent< SpriteRenderer >( );
+		anim = GetComponent< Animator >( );
+	}
+
+	void Start( ) {
+		rb2d.isKinematic = true;
+		generate_time = 0;
+		alpha = 0;
+
+		limit = false;
+		destroy_time = 10.0f;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		time -= Time.deltaTime;
+	void Update( ) {
+		GenerateUpdate( );
+		DestroyUpdate( );
+		AnimationUpdate( );
+	}
 
-		if( time < 0 ) {
+	void GenerateUpdate( ) {
+		generate_time += Time.deltaTime;
+
+		if ( generate_time < GEN_TIME ) {
+			alpha = generate_time / GEN_TIME;
+			color = sr.color;
+			color.a = alpha;
+			sr.color = color;
+		} else {
+			rb2d.isKinematic = false;
+		}
+	}
+
+	void DestroyUpdate( ) {
+		destroy_time -= Time.deltaTime;
+
+		if ( destroy_time < LIMIT_TIME ) {
+			limit = true;
+		}
+		if( destroy_time < 0 ) {
 			Destroy ( this.gameObject );
 		}
+	}
+
+	void AnimationUpdate( ) {
+		anim.SetBool( "Limit", limit );
 	}
 }
