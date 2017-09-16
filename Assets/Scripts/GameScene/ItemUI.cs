2a727cfe92ff84916aae3d91a53a552660bb05ba
iwa <iwa.com>
2017-09-16 15:52:14 +0900

    アイテムUIを表示できるようにした

---
+++ Assets/Scripts/GameScene/ItemUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUI : MonoBehaviour {
+
+	public Image image;
+
+	// Use this for initialization
+	void Start () {
+		image.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKey(KeyCode.A) ) {
+			image.enabled = true;
+		}
+		if( !Input.GetKey(KeyCode.A) ) {
+			image.enabled = false;
+		}
+		/*if( Input.GetButton( "LB" ) ) {
+			image.enabled = true;
+		}
+		if( !Input.GetButton( "LB" ) ) {
+			image.enabled = false;
+		}*/
+	}
+}
