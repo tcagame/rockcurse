5b430ec9b4a4d750a7fd4619974ca1323a8cbf1b
kent <k>
2017-09-18 22:08:52 +0900

    サンクス画面を10秒放置でも終了するようにしました

--- Assets/Scripts/GameScene/ThanksControl.cs
+++ Assets/Scripts/GameScene/ThanksControl.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class ThanksControl : MonoBehaviour {
 
+	const float EXIT_TIME = 10.0f;
+
+	float exit_time;
+
 	void Awake( ) {
 		new GameObject( ).AddComponent< SceneNavigator >( );
 	}
 
 	void Start( ) {
+		exit_time = EXIT_TIME;
 	}
 	
 	// Update is called once per frame
@@ -17,10 +22,13 @@ public class ThanksControl : MonoBehaviour {
 	}
 
 	private void inputUpdate( ) {
+		exit_time -= Time.deltaTime;
+
 		if ( Input.GetButtonDown("A") ||
 			Input.GetButtonDown("B") ||
 			Input.GetButtonDown("X") ||
-			Input.GetButtonDown("Y") ) {
+			Input.GetButtonDown("Y") ||
+			exit_time < 0 ) {
 			SceneNavigator.Instance.Change( "tgsVerTitle", 2.5f );
 		}
 	}
