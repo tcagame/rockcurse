ad7b3ec01df4eac0ee90b54a1de0259a3517690f
kent <k>
2017-09-18 20:56:35 +0900

    Thanksを差し替えました

--- Assets/Scripts/GameScene/ThanksControl.cs
+++ Assets/Scripts/GameScene/ThanksControl.cs
@@ -4,47 +4,16 @@ using UnityEngine;
 
 public class ThanksControl : MonoBehaviour {
 
-	SpriteRenderer sr;
-
-	Color color;
-
-	const float FADE_TIME = 0.0f;
-
-	float alpha;
-	float fade_time;
-	bool inputcut;
-
 	void Awake( ) {
-		sr = GetComponent< SpriteRenderer >( );
 		new GameObject( ).AddComponent< SceneNavigator >( );
 	}
 
 	void Start( ) {
-		alpha = 0;
-		fade_time = FADE_TIME;
-		inputcut = true;
 	}
 	
 	// Update is called once per frame
 	void Update( ) {
-		fadeinUpdate( );
-		if ( !inputcut ) {
-			inputUpdate( );
-		}
-	}
-
-	private void fadeinUpdate( ) {
-		fade_time += Time.deltaTime;
-
-		if ( fade_time < FADE_TIME ) {
-			alpha = fade_time / FADE_TIME;
-			color = sr.color;
-			color.a = alpha;
-			sr.color = color;
-		}
-		if ( fade_time > FADE_TIME ) {
-			inputcut = false;
-		}
+		inputUpdate( );
 	}
 
 	private void inputUpdate( ) {
